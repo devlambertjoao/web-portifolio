eddcd07fab7071dc93d6915a320e69b172f1e6b5
Joao Lambert <joao.lambert@gft.com>
2021-11-30 08:34:56 -0300

    Cover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import S from '../styles/Home.module.scss'
+import coverImage from "../public/assets/cover.png";
+import profileImage from "../public/assets/profile.png";
+import { useEffect, useRef } from 'react';
 
 const Home: NextPage = () => {
+  const coverRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    if(coverRef.current)
+      coverRef.current.style.filter = `blur(${window.scrollY / 128}px)`
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  })
+
   return (
     <div className={S.container}>
       <Head>
@@ -12,9 +28,41 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={S.main}>
-        <div className={S.content}>
-          testing
+        <div ref={coverRef} className={S.coverContainer}> 
+          <Image src={coverImage} alt="Cover Image" placeholder="blur" className={S.coverImage} layout="fill" 
+            objectPosition="center" objectFit="cover" />
+          <Image src={profileImage} alt="Site Author Profile Image" width={256} height={256} className={S.profileImage} />
         </div>
+        <div className={S.content}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam veritatis sed eum sequi veniam, error, repudiandae consectetur voluptatem voluptatibus doloremque ex dolorum, natus quo odit iusto ratione facere vitae.<br></br>
+        </div>
+        
       </main>
     </div>
   )
