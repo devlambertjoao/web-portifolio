4225fdcee65c780c9d850edcd80609aeb514ae57
Joao Lambert <joao.lambert@gft.com>
2022-02-01 19:28:44 -0300

    Class 5

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,16 +1,40 @@
 import { GetStaticPropsResult, NextPage } from "next";
+import S from "../../styles/Post.module.scss";
 
+//We use typescript, let type this...
 interface Props {
+  //Your component params
   id: number;
 }
 
 interface Context {
-  params: Props;
+  params: Props; //Url params
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          id: "5555",
+          //Always string here... The post with id 5555 will cache on build time,
+          //like an static page -> site.com/post/5555
+        },
+      },
+    ], //Register default pages, for cache on build time...
+    fallback: "blocking", //Make the page wait for load all data
+  };
 }
 
 export async function getStaticProps(
   context: Context
 ): Promise<GetStaticPropsResult<Props>> {
+  //get your data from backend here. Id is the component input, but can search an object
+  //from any API, and pass all object to PostPage Component
+  //On next video I can show this...
+  //Simulate delay of a request
+
+  await createDelay();
   const id: number = context.params.id;
 
   return {
@@ -20,15 +44,17 @@ export async function getStaticProps(
   };
 }
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
+const createDelay = () => new Promise((res) => setTimeout(res, 2800));
 
-const Post: NextPage<Props> = ({ id }) => {
-  return <div>Asadasdas {id}</div>;
+const PostPage: NextPage<Props> = ({ id }) => {
+  //Typescript <3 Working fine!!!
+
+  return (
+    <div className={S.container}>
+      My Post Page with id: {id}
+      <div>Lets publish this for see this feature working fine</div>
+    </div>
+  );
 };
 
-export default Post;
+export default PostPage;
